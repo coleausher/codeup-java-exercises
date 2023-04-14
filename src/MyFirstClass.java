d9ebbd4582e7ada9e7786d9901cbc0d44d10b801
Cole Usher <coleausher97@gmail.com>
2023-04-14 14:38:17 -0500

    Java intro Lecture

---
+++ src/MyFirstClass.java
@@ -0,0 +1,11 @@
+public class MyFirstClass {
+// shortcut: psvm + tab orrrrr main + tab for " public static void main(String[] args){} "
+    public static void main(String[] args){
+        // this will log stuff in your command line
+        // strings in java must be in double quotes " " (single quotes ' ' are reserved for single characters)
+        // you must put a semicolon after every statement or else you will get a syntax error
+        // shortcut is sout + tab for "System.out.println"
+        System.out.println("Hello, Welcome to Java!");
+    }
+
+}
