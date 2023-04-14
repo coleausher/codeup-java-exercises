95516dbd831c8bb4997d5caafb6103eeccdb07d4
Cole Usher <coleausher97@gmail.com>
2023-04-14 16:25:40 -0500

    Syntax Exercises complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,65 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
+
+// Syntax Exercise HERE
+
+    int myFavoriteNumber = 25;
+
+    String myString = "My name is Cole";
+        System.out.println(myString);
+        myString = "character value";
+        myString = "3.14159";
+
+//    long myNumber;
+//        myNumber = 3.14;
+//        System.out.println(myNumber);
+//        myNumber = 123L;
+//        myNumber = 123;
+
+    float myNumber = 3.14F;
+
+    // starts at 5 and increments to 6 bc ++ is after x
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+    // starts at 6 because x is pre incrementing because ++ is before the x
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // class is not valid to set as a variable
+//        class hello = "hello codeup";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        // variable three is alreeady declared above ////  cannot cast 'string' into an 'int'
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//           int x = 4 + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//                int x = 3;
+//                int y = 4 * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+                int x = 10 / 2;
+                int y = 2 - 10;
+
+  // max value for byte is   byte (-128 to 127)
+        // error says required type is 'byte' but i have entered a 'int' value bc its bigger than the byte's range of (-128 to 127)
+                // byte num = 150;
+
+
     }
 }
