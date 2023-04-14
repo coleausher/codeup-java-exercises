d48a44b934533e33c246e0003d4814a3aeda2c21
Cole Usher <coleausher97@gmail.com>
2023-04-14 16:52:38 -0500

    Java Syntax Lecture

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,18 +5,24 @@ public class HelloWorld {
 // Syntax Exercise HERE
 
     int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber);
 
     String myString = "My name is Cole";
         System.out.println(myString);
-        myString = "character value";
-        myString = "3.14159";
+        // ' ' single quotes is for char only
+            //myString = 'c';
+        // cant store number in a string
+            //myString = 3.14159;
 
 //    long myNumber;
+        // decimals can only be in double
 //        myNumber = 3.14;
 //        System.out.println(myNumber);
+
 //        myNumber = 123L;
 //        myNumber = 123;
 
+    // put in F to signify float
     float myNumber = 3.14F;
 
     // starts at 5 and increments to 6 bc ++ is after x
@@ -29,32 +35,31 @@ public class HelloWorld {
 //        System.out.println(++x);
 //        System.out.println(x);
 
-        // class is not valid to set as a variable
+        // class is not valid to set as a variable because it is a keyword
 //        class hello = "hello codeup";
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
 //        int three = (int) o;
 
-        // variable three is alreeady declared above ////  cannot cast 'string' into an 'int'
+        // variable three is already declared above ////  cannot cast 'string' into an 'int'
 //        int three = (int) "three";
 
 //        int x = 4;
 //        x = x + 5;
-//           int x = 4 + 5;
+//           x += 5
 
 //        int x = 3;
 //        int y = 4;
 //        y = y * x;
-//                int x = 3;
-//                int y = 4 * x;
+//               y *= x;
 
 //        int x = 10;
 //        int y = 2;
 //        x = x / y;
 //        y = y - x;
-                int x = 10 / 2;
-                int y = 2 - 10;
+                // x/= y;
+                // y -= x;
 
   // max value for byte is   byte (-128 to 127)
         // error says required type is 'byte' but i have entered a 'int' value bc its bigger than the byte's range of (-128 to 127)
