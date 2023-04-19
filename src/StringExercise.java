94215e627bd261ba9ac98e5d3e467d5250ec9877
Cole Usher <coleausher97@gmail.com>
2023-04-19 10:11:50 -0500

    Strings Exercise complete

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String pink = "We don't need no education";
+        String floyd = "We don't need no thought control";
+        System.out.println(pink + " " + floyd);
+
+        String check = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(check);
+
+        String windows = "In windows, the main drive is usually C:\\\n";
+        System.out.println(windows);
+
+        String backslash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!\n";
+        System.out.println(backslash);
+    }
+}
+
