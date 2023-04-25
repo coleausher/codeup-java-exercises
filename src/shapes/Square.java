66c1b8eb7289c04b6c6149851212df90a2b2cec4
Cole Usher <coleausher97@gmail.com>
2023-04-25 11:23:58 -0500

    Inheritance And Polymorphism exercise complete

---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle {
+    public Square(int side) {
+        super(side, side);
+    }
+
+
+    //@Override
+    public int getArea() {
+        return (int) Math.pow(length, 2);
+    }
+
+   // @Override
+    public int getPerimeter() {
+        return 4 * length;
+    }
+}
