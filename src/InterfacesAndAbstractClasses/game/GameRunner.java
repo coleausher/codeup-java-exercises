34fc378051e54089448bb0674b8446c87d20efe8
Cole Usher <coleausher97@gmail.com>
2023-04-26 10:26:23 -0500

    Interfaces and Abstract Classes Lecture

--- src/InterfacesAndAbstractClasses/game/GameRunner.java
+++ src/InterfacesAndAbstractClasses/game/GameRunner.java
@@ -1,23 +1,23 @@
 package InterfacesAndAbstractClasses.game;
 
-public class GameRunner {
+//public class GameRunner {
 //    private MarioGame game;
 //    private SuperContraGame game;
 //    private GamingConsole game;
-    private PackmanGame game;
+//    private PackmanGame game;
 
-}
+//}
 //public GameRunner(MarioGame game) {
 //    this.game = game;
 //}
 
-    public GameRunner(PackmanGame game) {
-    this.game = game;
-    }
+//    public GameRunner(PackmanGame game) {
+//    this.game = game;
+//    }
 
-public void run(){
-    game.up();
-    game.down();
-    game.left();
-    game.right();
-}
+//public void run(){
+//    game.up();
+//    game.down();
+//    game.left();
+//    game.right();
+//}
